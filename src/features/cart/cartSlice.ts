7204f155d4bdb0afc9143af705a2a253572381e0
Kamil Michalski <kmichalski@outlook.com>
2022-11-29 18:05:40 +0100

    items total qunatity

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -47,8 +47,32 @@ export const { addItem, removeItem } = cartSlice.actions;
 
 export const selectItemsQuantity = (state: RootState) => {
   // TODO 4: Zmodyfikuj selector, tak aby zliczał prawidłowo elementy
-  return state.cart.items.length;
+  // let total = 0;
+
+  // Junior Level
+  // for (let i = 0; i < state.cart.items.length; i++) {
+  //   total += state.cart.items[i].quantity;
+  // }
+
+  // Junior+ Level
+  // state.cart.items.forEach((item) => {
+  //   total += item.quantity;
+  // });
+
+  // Medium level
+
+  // [ { qunatity: 2 }, { qunatity: 3 }]
+
+  const total = state.cart.items.reduce((acc, item) => {
+    acc += item.quantity;
+    return acc; // (1) acc = 0 => 2; (2) acc = 2 => 2 + 3 = 5
+  }, 0); // acc = 0;
+
+  return total;
 };
+
+// TODO: Utwórz selector który zwróci całkowitą sumę koszyka
+// I wyświetl w koszyku ( komponent Cart )
 
 export const selectItems = (state: RootState) => {
   return state.cart.items;
